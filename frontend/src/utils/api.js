774c96c2496c0db4b37bcef0e17b9fb96a996759
rishigogia1 <rishigogia30@gmail.com>
2026-01-01 19:03:33 +0530

    fix: force frontend to use backend api url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,9 +2,11 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-// Attach token automatically
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
